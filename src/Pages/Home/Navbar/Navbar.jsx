b8842543145e06ce516086fa1c6fac5df043aa3e
sohelranagtc <sohelranagtc@gmail.com>
2023-07-24 19:53:36 +0600

    ad navbar

---
+++ src/Pages/Home/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+
+    const navbar =
+        <>
+            <a className='m-5'><Link to="/">Home</Link></a>
+            <a><Link to="/colleges">Colleges</Link></a>
+        </>
+
+    return (
+        <div className="navbar justify-center bg-base-100">
+            <div className="flex-1">
+                <a className="btn btn-ghost uppercase text-xl">World Colleges</a>
+            </div>
+            <div className='text-center'>
+                {navbar}
+            </div>
+            <div className="flex-none gap-2">
+                <div className="form-control">
+                    <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+                </div>
+                <div className="dropdown dropdown-end">
+                    <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full">
+                            <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                        </div>
+                    </label>
+                    <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                        <li>
+                            <a className="justify-between">
+                                Profile
+                                <span className="badge">New</span>
+                            </a>
+                        </li>
+                        <li><a>Settings</a></li>
+                        <li><a>Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
